60f91cdc3a86f6e14a78ef26f9ebeec42c36ba29
Paulo Santos <paulo.f.santos@glintt.com>
2026-02-25 22:23:25 +0000

    fix bug

--- packages/api/src/modules/reference/application/queries/get-languages-query.handler.ts
+++ packages/api/src/modules/reference/application/queries/get-languages-query.handler.ts
@@ -5,20 +5,27 @@ import { Inject } from '@nestjs/common';
 import { REFERENCE_CACHE_CONFIG } from '../../reference.constants';
 import { GetLanguagesQuery } from './get-languages.query';
 import { CacheService } from '../../../shared/domain/services/cache.service';
+import { ConfigService } from '@nestjs/config';
 
 @QueryHandler(GetLanguagesQuery)
 export class GetLanguagesQueryHandler implements IQueryHandler<
   GetLanguagesQuery,
   LanguageDto[]
 > {
+  private readonly tmdbClient: TmdbClient;
+
   constructor(
+    private readonly configService: ConfigService,
     private readonly cacheService: CacheService,
-    private readonly tmdbClient: TmdbClient,
     @Inject(REFERENCE_CACHE_CONFIG)
     private readonly cacheConfig: {
       languagesTtl: number;
     },
-  ) {}
+  ) {
+    this.tmdbClient = new TmdbClient(
+      this.configService.get<string>('TMDB_API_KEY', ''),
+    );
+  }
 
   public async execute(_: GetLanguagesQuery): Promise<LanguageDto[]> {
     return await this.cacheService.getOrSet(
